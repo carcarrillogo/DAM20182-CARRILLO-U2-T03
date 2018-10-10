aea8a452fb4dba9d521af5264f52303fe01f6597
carcarrillogo <caarcarrillogo@ittepic.edu.mx>
2018-10-10 09:04:55 -0600

    primer commit

---
+++ src/pages/otherPage/otherPage.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+    selector: 'page-other',
+    templateUrl: 'otherPage.html',
+})
+export class OtherPage {
+    constructor(public navCtrl: NavController, public navParams:
+        NavParams) {
+    }
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad OtherPage');
+    }
+    goBack() {
+        this.navCtrl.pop();
+    }
+}
